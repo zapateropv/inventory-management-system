c8cffbd868871de5559f52f2875baf245e165690
zapateropv <paulvincentzapatero17@gmail.com>
2026-08-08 21:32:15 +0800

    implemented shadcn

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+document.documentElement.classList.add("dark");
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
